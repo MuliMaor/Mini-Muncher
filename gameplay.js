6dc0d7890d241f4b517f00a5922747bfdd01b34d
MuliMaor <99132799+MuliMaor@users.noreply.github.com>
2024-03-11 18:54:15 +0200

    Add files via upload

---
+++ gameplay.js
@@ -0,0 +1,87 @@
+{
+	"levelsData": [
+{
+			"id": 1,
+			"size": 6,
+			"pieces": [
+				{"type": "HERO", "posX": 2, "posY": 4},
+				{"type": "BLOCK3H", "posX": 2, "posY": 1},
+{"type": "BLOCK2H", "posX": 4, "posY": 2},
+{"type": "BLOCK2H", "posX": 0, "posY": 4},
+{"type": "BLOCK3H", "posX": 2, "posY": 5},
+				{"type": "BLOCK2V", "posX": 0, "posY": 3},
+{"type": "BLOCK3V", "posX": 1, "posY": 2},
+{"type": "BLOCK3V", "posX": 3, "posY": 4},
+{"type": "BLOCK2V", "posX": 4, "posY": 4},
+{"type": "BLOCK2V", "posX": 5, "posY": 1}
+			]
+		},
+		{
+			"id": 2,
+			"size": 6,
+			"pieces": [
+				{"type": "HERO", "posX": 2, "posY": 3},
+				{"type": "BLOCK3H", "posX": 2, "posY": 0},
+{"type": "BLOCK2H", "posX": 2, "posY": 1},
+{"type": "BLOCK3H", "posX": 2, "posY": 4},
+{"type": "BLOCK2H", "posX": 1, "posY": 5},
+{"type": "BLOCK3H", "posX": 3, "posY": 5},
+                       {"type": "BLOCK2V", "posX": 0, "posY": 2},
+{"type": "BLOCK3V", "posX": 0, "posY": 5},
+{"type": "BLOCK2V", "posX": 1, "posY": 1},
+{"type": "BLOCK2V", "posX": 1, "posY": 4},
+{"type": "BLOCK3V", "posX": 4, "posY": 3},
+{"type": "BLOCK2V", "posX": 5, "posY": 1},
+{"type": "BLOCK2V", "posX": 5, "posY": 3}
+			]
+		},
+{
+			"id": 3,
+			"size": 6,
+			"pieces": [
+				{"type": "HERO", "posX": 2, "posY": 2},
+				{"type": "BLOCK2H", "posX": 1, "posY": 0},
+{"type": "BLOCK3H", "posX": 3, "posY": 0},
+{"type": "BLOCK2H", "posX": 0, "posY": 2},
+{"type": "BLOCK3H", "posX": 3, "posY": 3},
+{"type": "BLOCK2H", "posX": 1, "posY": 4},
+                       {"type": "BLOCK2V", "posX": 0, "posY": 1},
+{"type": "BLOCK2V", "posX": 3, "posY": 2},
+{"type": "BLOCK2V", "posX": 4, "posY": 5}
+		]
+		},
+
+		{
+			"id": 4,
+			"size": 5,
+			"pieces": [
+				{"type": "HERO", "posX": 1, "posY": 3},
+				{"type": "BLOCK3H", "posX": 0, "posY": 0},
+{"type": "BLOCK2H", "posX": 0, "posY": 1},
+{"type": "BLOCK2H", "posX": 2, "posY": 4},
+                       {"type": "BLOCK2V", "posX": 0, "posY": 4},
+{"type": "BLOCK3V", "posX": 2, "posY": 3},
+{"type": "BLOCK3V", "posX": 3, "posY": 2},
+{"type": "BLOCK2V", "posX": 4, "posY": 1},
+{"type": "BLOCK2V", "posX": 4, "posY": 3}
+			]
+		},
+		{
+			"id": 5,
+			"size": 6,
+			"pieces": [
+				{"type": "HERO", "posX": 2, "posY": 5},
+				{"type": "BLOCK2V", "posX": 0, "posY": 1},
+				{"type": "BLOCK3V", "posX": 0, "posY": 5},
+{"type": "BLOCK2V", "posX": 3, "posY": 4},
+{"type": "BLOCK2V", "posX": 4, "posY": 3},
+{"type": "BLOCK3V", "posX": 5, "posY": 2},
+{"type": "BLOCK2V", "posX": 5, "posY": 4},
+                       {"type": "BLOCK2H", "posX": 0, "posY": 2},
+{"type": "BLOCK3H", "posX": 1, "posY": 0},
+{"type": "BLOCK2H", "posX": 1, "posY": 3},
+{"type": "BLOCK2H", "posX": 4, "posY": 5}
+			]
+		}
+	]
+}
